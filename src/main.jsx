f2207498b17dd603827e3463c000eb03fbe4c36d
aronjobert <aronjobertsalim@gmail.com>
2026-04-09 12:09:14 +0800

    added vercel analytics insight

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { Analytics } from '@vercel/analytics/react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
