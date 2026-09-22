45ed0e67a502ac6cffb78879dfaf0dc232e52793
aronjobert <aronjobertsalim@gmail.com>
2026-09-22 20:20:00 +0800

    update site title and added vercel web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import LenisScroll from './components/LenisScroll'
 import CustomDevelopment from './components/CustomDevelopment'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
     return (
         <>
+            <Analytics />
             <LenisScroll />
             <Navbar />
             <Header />
